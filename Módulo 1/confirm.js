8737cbd20bd6631b1c7bebe8201a499c1a47af49
Andressa Sias <sias.andressa@gmail.com>
2018-12-28 16:10:05 -0200

    Add files via upload

---
+++ Módulo 1/confirm.js
@@ -0,0 +1,4 @@
+var sair = confirm ("Confirma ir para a página do google?");
+if(sair == 1){
+	window.location = "http://www.google.com.br";
+}
---
+++ Módulo 1/meucodigo.js
@@ -0,0 +1,8 @@
+var a = 2;
+var b = 9;
+var c = a + b;
+alert("O total da soma é " + c);
+
+var nome;
+nome = prompt("Digita teu nome aê");
+alert("Eaí "+nome+"! Tudo tranquilo?");
